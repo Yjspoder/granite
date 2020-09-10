20f3a6b047ec9ba72ea3dc966ca7d4f402fbcade
yjspoder <injose.joshi@gmail.com>
2020-09-11 02:09:26 +0530

    Added constraint to make description not nullable

---
+++ db/migrate/20200910203713_make_description_nuot_nullable.rb
@@ -0,0 +1,5 @@
+class MakeDescriptionNuotNullable < ActiveRecord::Migration[6.0]
+  def change
+    change_column_null :tasks, :description, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_135159) do
+ActiveRecord::Schema.define(version: 2020_09_10_203713) do
 
   create_table "tasks", force: :cascade do |t|
-    t.text "description"
+    t.text "description", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
